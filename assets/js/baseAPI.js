81ba41b0ca9ab847f5b45d4e11c07f4f43701d47
lrqtt <lurui774862045@163.com>
2022-09-23 14:31:21 +0800

    完成文章管理相关功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,7 +4,7 @@
 
 $.ajaxPrefilter(function(options) {
   //拼接根路径
-  options.url = 'http://www.liulongbin.top:3007' + options.url
+  options.url = 'http://api-breakingnews-web.itheima.net' + options.url
   console.log(options.url)
 
   //统一为有权限的接口，设置请求头
